c00843aee89991ecd0fb996f1e6b3fab0c1dff41
Alok Singh <alok.singh@razorpay.com>
2022-01-15 20:45:39 +0530

    Changed object name from abhijeet to s2

--- src/oop/StudentUse.java
+++ src/oop/StudentUse.java
@@ -6,8 +6,8 @@ public class StudentUse {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         Student s1 = new Student("Shefali", 24, 123);
-        Student abhijeet = new Student("Abhijeet", 24, 121);
+        Student s2 = new Student("Abhijeet", 24, 121);
         s1.print();
-        abhijeet.print();
+        s2.print();
     }
 }
